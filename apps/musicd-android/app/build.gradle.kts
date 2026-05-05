688f1878355f3d44be5f21015c962847644a54fc
Andrew Rumble <github@andrewrumble.com>
2026-05-05 19:09:26 +0100

    chore(app): bump version

--- apps/musicd-android/app/build.gradle.kts
+++ apps/musicd-android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 10000
-        versionName = "1.1.0"
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
